8f442549241e82f2f157882c3a72c1a215b60c06
hamidisaac <isaac.hamid@gmail.com>
2023-01-23 02:20:23 +0330

    device model

--- challenge/api/models.py
+++ challenge/api/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
-class Device():
-    def __init__(self, name):
-        self.name = name
+class Device(models.Model):
+	myid = models.CharField(max_length=100)
+	deviceModel = models.CharField(max_length=100)
+	name = models.CharField(max_length=100)
+	note = models.CharField(max_length=300)
+	serial = models.CharField(max_length=100)
