be26c384d2db8a43123cc2bd8d0420c477ec5617
hamidisaac <isaac.hamid@gmail.com>
2023-01-30 13:14:57 +0330

    dynamodb model

---
+++ challenge/api/dynamo_models.py
@@ -0,0 +1,7 @@
+class Device():
+    def __init__(self, id, deviceModel, name, note, serial):
+        self.id = id
+        self.deviceModel = deviceModel
+        self.name = name
+        self.note = note
+        self.serial = serial
